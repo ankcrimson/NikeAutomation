7901103d61a65ba5adfc69b44524147e94406678
ankcrimson <ankcrimson@yahoo.com>
2015-08-20 21:28:31 +0530

    modified:   Nodejs_Tests/lib/helper.js 	modified:   Nodejs_Tests/nightwatch.json 	modified:   Nodejs_Tests/reports/CHROME_43.0.2357.93_ANDROID_nike_guest_checkout.xml 	new file:   Nodejs_Tests/reports/CHROME_44.0.2403.133_ANDROID_nike_guest_checkout.xml 	modified:   Nodejs_Tests/tests/nike_guest_checkout.js

--- Nodejs_Tests/lib/helper.js
+++ Nodejs_Tests/lib/helper.js
@@ -1,7 +1,7 @@
 module.exports = {
 	  wait_click: function (browser,tag) {
       // whatever
-      browser.waitForElementVisible(tag, 10000)
+      browser.waitForElementVisible(tag, 20000)
       .click(tag)
       return this;
    },
@@ -10,5 +10,18 @@ module.exports = {
    	browser.executeAsync(str)
    	.pause(3000)
    	return this
+   },
+
+   wait_setValue: function(browser,tag,str)
+   {
+   	browser.waitForElementVisible(tag, 10000)
+   	.setValue(tag,str)
+   	 return this
+   },
+
+   pause: function(browser,timee)
+   {
+   	browser.pause(browser,timee)
+   	 return this
    }
     };
--- Nodejs_Tests/tests/nike_guest_checkout.js
+++ Nodejs_Tests/tests/nike_guest_checkout.js
@@ -2,8 +2,10 @@ var helper=require('../lib/helper.js')
 module.exports = {
   'Guest Checkout FUnctionality' : function (browser) {
     browser
-    .url('http://store.nike.com');
-    helper.wait_click(browser,'#footer > div.non-employee-user > div > section > div > div > div > div > div.nike-cq-footer-three > div > div > div.nike-cq-list > ul > li > a')
+  //  .url('http://store.nike.com');
+    .url('http://www.nike.com/language_tunnel');   
+    helper
+ //   .wait_click(browser,'#footer > div.non-employee-user > div > section > div > div > div > div > div.nike-cq-footer-three > div > div > div.nike-cq-list > ul > li > a')
     .wait_click(browser,'body > div.tunnel-content.nsg-font-family--platform > div.region-tunnel-content > div.languages > div.regions.Americas > div > ul > ul > li.size-medium.US > a')
     .wait_click(browser,'#main-content > div > div > div > div > section.nike-cq-page-module.nike-cq-page-menu.nike-cq-mod-pos-2 > div > div > div > div > div > div > div > ul > li.nike-cq-page-menu-item.nike-cq-page-menu-item1.nike-cq-page-menu-item-level1.hide > a')
     .wait_click(browser,'#main-content > div > div > div > div > section.nike-cq-page-module.nike-cq-page-menu.nike-cq-mod-pos-2 > div > div > div > div > div > div > div > ul > li.nike-cq-page-menu-item.nike-cq-page-menu-item1.nike-cq-page-menu-item-level1 > div > ul > li.nike-cq-page-menu-item.nike-cq-page-menu-item2.nike-cq-page-menu-item-level2.hide > a')
@@ -13,7 +15,17 @@ module.exports = {
     .wait_click(browser,'#pdp-size-select')
     .executeAsync(browser,'document.getElementById("frm-size-select").selectedIndex=1')
     .wait_click(browser,'#add-to-cart-btn')
-    
+    .pause(browser,3000)
+    .wait_click(browser,'#header > div.tier0 > a.cart')
+    .wait_click(browser,'div.cartButton')
+    .wait_setValue(browser,'#fname','Capt Jack')
+    .wait_setValue(browser,'#lname','Sparrow')
+    .wait_setValue(browser,'#address1Field','Black Perl')
+    .wait_setValue(browser,'#address2Field','Caribbean Sea')
+    .wait_setValue(browser,'#cityField','Beaverton')
+    .wait_setValue(browser,'#postalCodeField','97005')
+    .wait_setValue(browser,'#shippingEmailField','captian@blackperl.org')
+
 //    .setValue('input[type=text]', 'nightwatch')
 //    .waitForElementVisible('button[name=btnG]', 1000)
 //    .click('button[name=btnG]')
