6dd073e64a0ba1a4f281459efdd1de5b2d39e98d
ankcrimson <ankcrimson@yahoo.com>
2015-08-21 22:40:01 +0530

    updated

--- Nodejs_Tests/lib/helper.js
+++ Nodejs_Tests/lib/helper.js
@@ -1,27 +1,46 @@
 module.exports = {
 	  wait_click: function (browser,tag) {
       // whatever
+      try{
       browser.waitForElementVisible(tag, 20000)
+      .waitForElementNotVisible('div.loading-overlay',10000,false)
       .click(tag)
+      }catch (err) {
+      console.error('catastrophe', err);
+      }
       return this;
    },
 
    executeAsync: function(browser,str){
-   	browser.executeAsync(str)
-   	.pause(3000)
-   	return this
+   	try{
+      browser.executeAsync(str)
+   	//.pause(3000)
+   	}catch (err) {
+      console.error('catastrophe', err);
+      }
+      
+      return this
    },
 
    wait_setValue: function(browser,tag,str)
    {
-   	browser.waitForElementVisible(tag, 10000)
+   	try{
+      browser.waitForElementVisible(tag, 10000)
    	.setValue(tag,str)
-   	 return this
+   	}catch (err) {
+      console.error('catastrophe', err);
+      }
+      
+       return this
    },
 
-   pause: function(browser,timee)
+   pause_1: function(browser,timee)
    {
-   	browser.pause(browser,timee)
-   	 return this
+   	try{
+      browser.pause(browser,timee)
+   	}catch (err) {
+      console.error('catastrophe', err);
+      }
+      return this
    }
     };
